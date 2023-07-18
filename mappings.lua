b2b61d7afbc207c020dc5730b6111d69f7236896
Jesús Molano <jessumolano@gmail.com>
2023-07-18 18:32:04 +0100

    Added Short shortcut

--- mappings.lua
+++ mappings.lua
@@ -5,13 +5,16 @@
 -- automatically pick-up stored data by this setting.)
 return {
   -- first key is the mode
+  -- n = normal, v = visual, i = insert, c = command, t = terminal
   n = {
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+        )
       end,
       desc = "Pick to close",
     },
@@ -19,10 +22,14 @@ return {
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
-    ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<C-s>"] = { ":w!<cr>", desc = "Save File" },
   },
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
+  },
+  v = {
+    -- sort lines
+    ["<C-S>"] = { ":!sort<cr>gv", desc = "Sort Lines" },
   },
 }
